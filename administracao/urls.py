1013b6d41851a6a389bb40041a10a552812d3a15
machadodan <danielg_m@hotmail.com>
2022-11-30 19:39:01 -0300

    Cadastro de usuarios finalizado

--- administracao/urls.py
+++ administracao/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import servico_views, usuarios_views
 urlpatterns = [
-    path('servicos/cadastrar', cadastrar_servico, name='cadastrar_servico'),
-    path('servicos/listar', listar_servicos, name='listar_servicos'),
-    path('servicos/editar/<int:id>', editar_servico, name='editar_servico'),
+    path('servicos/cadastrar', servico_views.cadastrar_servico, name='cadastrar_servico'),
+    path('servicos/listar', servico_views.listar_servicos, name='listar_servicos'),
+    path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
+    path('usuarios/cadastrar', usuarios_views.cadastrar_usuario, name='cadastrar_usuario'),
 ]
