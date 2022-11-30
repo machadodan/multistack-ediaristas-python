a8b8c9760bedf228e222ecd1e725c23c7244c4b1
machadodan <danielg_m@hotmail.com>
2022-11-30 20:35:11 -0300

    Listagem de usuarios finalizada

--- administracao/urls.py
+++ administracao/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('servicos/listar', servico_views.listar_servicos, name='listar_servicos'),
     path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
     path('usuarios/cadastrar', usuarios_views.cadastrar_usuario, name='cadastrar_usuario'),
+    path('usuarios/listar', usuarios_views.listar_usuarios, name='listar_usuarios'),
+
 ]
--- administracao/views/usuarios_views.py
+++ administracao/views/usuarios_views.py
@@ -1,11 +1,20 @@
 from ..forms.usuario_forms import UsuarioForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
 def cadastrar_usuario(request):
     if request.method == "POST":
         form_usuario = UsuarioForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
+            return redirect('listar_usuarios')
     else:
         form_usuario = UsuarioForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+    ## metodo para listar usuarios
+def listar_usuarios(request):
+    ## pego o model que o projeto ta usando como autenticação e partir dele faço a consulta
+    User = get_user_model()
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
