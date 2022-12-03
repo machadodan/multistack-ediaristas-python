4e5bb9731e506c0a55c713fcb2d560f008e72ad5
machadodan <danielg_m@hotmail.com>
2022-12-02 21:47:27 -0300

    Modulo de autenticação finalizada

--- administracao/views/usuarios_views.py
+++ administracao/views/usuarios_views.py
@@ -1,7 +1,9 @@
 from ..forms.usuario_forms import CadastroUsuarioForm, EditarUsuarioForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def cadastrar_usuario(request):
     if request.method == "POST":
         form_usuario = CadastroUsuarioForm(request.POST)
@@ -13,12 +15,14 @@ def cadastrar_usuario(request):
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
 
     ## metodo para listar usuarios
+@login_required
 def listar_usuarios(request):
     ## pego o model que o projeto ta usando como autenticação e partir dele faço a consulta
     User = get_user_model()
     usuarios = User.objects.filter(is_superuser=True)
     return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
 
+@login_required
 def editar_usuario(request, id):
     User = get_user_model()
     usuario = User.objects.get(id=id)
